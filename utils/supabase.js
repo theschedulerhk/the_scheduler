288121779cc73c887d6b0804ef142fb970404c6f
theschedulerhk <theschedulerhk@outlook.com>
2026-07-07 11:27:16 +0800

    Update supabase.js

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace with your actual project keys from your Supabase dashboard settings
-const SUPABASE_URL = "https://supabase.co";
-const SUPABASE_ANON_KEY = "your-anon-public-key";
+const SUPABASE_URL = "https://dipefqpepuiemvcyvaya.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "sb_publishable_wWCPZt3H78e9FbPyVQLWfA__c2LjvB6";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
