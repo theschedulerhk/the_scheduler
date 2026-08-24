6d4bf8177fbc9f9c81813ae45e3109d6cc0b2814
theschedulerhk <theschedulerhk@outlook.com>
2026-08-24 23:07:05 +0800

    Update page.js

--- app/tracking/new/page.js
+++ app/tracking/new/page.js
@@ -1,5 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
+import Link from 'next/link'; 
 import { supabase } from '../../../utils/supabase';
 
 export default function NewApplicationForm({ lang = 'en' }) {
